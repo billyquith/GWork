378a9284e9d23a87cda9e796819c5efc7006fe4c
BQ <chinbillybilbo@gmail.com>
2013-09-06 20:39:08 +0100

    Fix Xcode4 premake files * Can generate an Xcode4 solution and build Allegro sample without errors. * GWork requires C++11. Add this to the build options. * Added "user.lua" to save user editing build files. * Comments and lua table args for clarity.

--- gwen/Projects/inc/macosx.lua
+++ gwen/Projects/inc/macosx.lua
@@ -1,18 +1,27 @@
 
-
 if MONOLITHIC then
     ALLEGRO_LIBS	= { "UnitTest", "Renderer-Allegro", "GWEN-Static", "alleg" }
     ALLEGRO_LIBS_D	= { "UnitTest", "Renderer-Allegro", "GWEN-Static", "alleg" }
 else
     -- Separate static libraries
-    function allib(name) return "allegro" .. name .. "-static" end
+    local function allib(name) return "allegro" .. name .. "-static" end
 
-    ALLEGRO_LIBS	= { "UnitTest", "Renderer-Allegro", "GWEN-Static",
-                        "Cocoa.framework", "IOKit.framework", "OpenGL.framework",
-                        "OpenAL.framework", "AudioToolbox.framework",
-                        "freetype", "bz2", "z",
-                         allib(""), allib("_main"), allib("_ttf"), allib("_font"),
-                         allib("_primitives"), allib("_image") }
+    ALLEGRO_LIBS =
+    {
+        -- OSX Cocoa Libraries
+        "UnitTest", "Cocoa.framework", "IOKit.framework", "OpenGL.framework",
+        "OpenAL.framework", "AudioToolbox.framework",
+
+        -- Allegro dependencies
+        "freetype", "bz2", "z",
+
+        -- Allegro Libs
+        allib(""), allib("_main"), allib("_ttf"), allib("_font"),
+        allib("_primitives"), allib("_image"),
+
+        -- GWEN Allegro
+        "Renderer-Allegro", "GWEN-Static"
+    }
 
     ALLEGRO_LIBS_D	= ALLEGRO_LIBS
 end
@@ -21,10 +30,12 @@ SFML_LIBS		= { "UnitTest", "Renderer-SFML", "GWEN-Static", "sfml-graphics",
                     "sfml-window", "sfml-system", "GL" }
 SFML_LIBS_D		= { "UnitTest", "Renderer-SFML", "GWEN-Static", "sfml-graphics",
                     "sfml-window", "sfml-system", "GL" }
-                    
+
 SFML2_LIBS		= { "UnitTest", "Renderer-SFML2", "GWEN-Static", "sfml-graphics",
                     "sfml-window", "sfml-system", "GL" }
 SFML2_LIBS_D	= { "UnitTest", "Renderer-SFML2", "GWEN-Static", "sfml-graphics",
-                    "sfml-window", "sfml-system", "GL" }                    
-                    
+                    "sfml-window", "sfml-system", "GL" }
+
 CROSS_LIBS 		= { "UnitTest", "Renderer-OpenGL_DebugFont", "GWEN-Static", "GL", "freeimage" }
+
+
