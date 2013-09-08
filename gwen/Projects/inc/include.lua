eac8811a208ee217718caa4cd637cb4f2c008ee4
BQ <chinbillybilbo@gmail.com>
2013-09-08 15:09:21 +0100

    Note on Windows paths.

--- gwen/Projects/inc/include.lua
+++ gwen/Projects/inc/include.lua
@@ -19,11 +19,12 @@ else
 
 -- User overrides. Comment in to use, otherwise ignored.
 user = {
-    -- User include/lib paths. E.g. look for dependencies
+    --- User include/lib paths. E.g. look for dependencies
+    --- Note: Use "/" for Windows, not "\".
     --includes = { "/my/path" },    -- User include paths.
     --libs = { "/my/path" },        -- User lib paths.
 
-    -- Options. These are ignored if not applicable.
+    --- Options. These are ignored if not applicable.
     --macports = true,       -- Use Macports on OSX.
     --al_monolithic = true,  -- Link to monolithic lib for Alllegro5.
 }
