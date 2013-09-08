ba00f733f72bd45eda90e16e6b7bde3208f7010f
BQ <chinbillybilbo@gmail.com>
2013-09-08 15:07:04 +0100

    Add Windows SDL2 libs * SDL2 paths specified in user.lua

--- gwen/Projects/inc/windows.lua
+++ gwen/Projects/inc/windows.lua
@@ -8,6 +8,10 @@ ALLEGRO_LIBS_D	= { "UnitTest", "Renderer-Allegro", "GWEN-Static",
 CROSS_LIBS 		= { "UnitTest", "Renderer-OpenGL_DebugFont", "GWEN-Static",
                     "opengl32", "freeimage" }
 
+SDL2_LIBS       = { "opengl32",
+                    "SDL2", "SDL2main", "SDL2_ttf", "SDL2_image",
+                    "UnitTest", "Renderer-SDL2", "GWEN-Static" }
+                    
 SFML_LIBS		= { "UnitTest", "Renderer-SFML", "GWEN-Static", "sfml-main",
                     "sfml-graphics-s", "sfml-window-s", "sfml-system-s",
                     "opengl32" }
