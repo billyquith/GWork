1449de64fff188185179bee688179a1c3c4b5171
BQ <chinbillybilbo@gmail.com>
2014-03-29 13:40:54 +0000

    Add designer. * Source code from Github project. With Allegro modifications I made.

---
+++ designer/src/Cage.cpp
@@ -0,0 +1,121 @@
+
+#include "Cage.h"
+
+using namespace Gwen;
+
+GWEN_CONTROL_CONSTRUCTOR( Cage )
+{
+	m_Control = NULL;
+	m_iBorder = 5;
+}
+
+void Cage::Render( Gwen::Skin::Base* skin )
+{
+	Gwen::Rect bounds = GetRenderBounds();
+
+	bounds.x += m_iBorder;
+	bounds.y += m_iBorder;
+	bounds.w -= m_iBorder * 2;
+	bounds.h -= m_iBorder * 2;
+
+	skin->GetRender()->SetDrawColor( Color( 130, 30, 30, 10 ) );
+	skin->GetRender()->DrawFilledRect( bounds );
+
+	skin->GetRender()->SetDrawColor( Color( 20, 150, 255, 255 ) );
+	skin->GetRender()->DrawLinedRect( bounds );
+	bounds.x += 1; bounds.y += 1; bounds.w -= 2; bounds.h -= 2; 
+	skin->GetRender()->DrawLinedRect( bounds );
+}
+
+void Cage::PostLayout( Skin::Base* skin )
+{
+	if ( !m_Control ) return;
+
+	Gwen::Point canvaspos = m_Control->LocalPosToCanvas();
+	Gwen::Point parentpos = GetParent()->CanvasPosToLocal( canvaspos );
+
+	parentpos.x -= m_iBorder;
+	parentpos.y -= m_iBorder;
+
+	SetPos( parentpos );
+
+	Gwen::Point size = m_Control->GetSize();
+	size.x += m_iBorder * 2;
+	size.y += m_iBorder * 2;
+
+	SetSize( size );
+}
+
+void Cage::Setup( Controls::Base* pControl )
+{
+	m_Control = pControl;
+}
+
+void Cage::SetDepressed( bool b )
+{
+	BaseClass::SetDepressed( b );
+
+	m_DragPoint = m_Control->GetPos();
+	m_bDragged = false;
+}
+
+void Cage::OnMouseMoved( int x, int y, int deltaX, int deltaY )
+{
+	if ( !IsDepressed() ) return;
+
+	Controls::Base*	pControlParent = m_Control->GetParent();
+	Gwen::Point pntRemainder = m_DragPoint - m_Control->GetPos();
+
+	//
+	// This event is used by the SelectionLayer to scan
+	// and change the parent - if appropriate.
+	//
+	{
+		Event::Information info;
+
+		info.Point = Gwen::Point( x, y );
+		onMoving.Call( this, info );
+	}
+
+	//
+	// If the parent changed then fix up the drag position
+	//
+	if ( pControlParent != m_Control->GetParent() )
+	{
+		m_DragPoint = m_Control->GetPos() + pntRemainder;
+
+	}
+
+	if ( !m_bDragged )
+	{
+		Event::Information info;
+		onDragStart.Call( this, info );
+	}
+	m_bDragged = true;
+
+	m_DragPoint += Gwen::Point( deltaX, deltaY );
+
+	Gwen::Point pos = m_DragPoint;
+
+	pos.x = ((int)((float)pos.x / 10.0f)) * 10;
+	pos.y = ((int)((float)pos.y / 10.0f)) * 10;
+
+	pos -= m_Control->GetPos();
+
+	{
+		Event::Information info;
+		info.Point = pos;
+		onMoved.Call( this, info );
+	}
+}
+
+void Cage::OnPress()
+{
+	if ( IsToggle() )
+	{
+		SetToggleState( !GetToggleState() );
+	}
+
+	if ( !m_bDragged )
+		onPress.Call( this );
+}
---
+++ designer/src/main.cpp
@@ -0,0 +1,37 @@
+
+#include "Gwen/Gwen.h"
+#include "Gwen/Controls.h"
+#include "Gwen/Skins/TexturedBase.h"
+#include "DesignerBase.h"
+#ifdef WIN32
+	#include "Gwen/Renderers/DirectX9.h"
+#else
+	#include "Gwen/Renderers/Allegro.h"
+#endif
+
+int main(int, char **)
+{
+#ifdef WIN32
+	Gwen::Renderer::DirectX9	renderer;
+#else
+	Gwen::Renderer::Allegro		renderer;
+#endif
+	Gwen::Skin::TexturedBase	skin( &renderer );
+	skin.SetRender( &renderer );
+
+	Gwen::Controls::WindowCanvas window_canvas( -1, -1, 1024, 600, &skin, "GWEN Designer" );
+	skin.Init( "img/DesignerSkin.png" );
+
+	skin.SetDefaultFont( L"OpenSans.ttf", 11 );
+
+	DesignerBase* pDesignerBase = new DesignerBase( &window_canvas );
+
+	while ( !window_canvas.WantsQuit() )
+	{
+		window_canvas.DoThink();
+	}
+	
+	delete pDesignerBase;
+	
+	return 0;
+}
