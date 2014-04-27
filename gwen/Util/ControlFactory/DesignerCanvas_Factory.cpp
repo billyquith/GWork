c5fcfeb591e07fba4424c70ef8e0bd19411c291a
BQ <chinbillybilbo@gmail.com>
2014-04-27 10:55:27 +0100

    Designer loading fix.  - Previous refactor broke loading.  - DocumentCanvas needed for load/save.

--- gwen/Util/ControlFactory/DesignerCanvas_Factory.cpp
+++ gwen/Util/ControlFactory/DesignerCanvas_Factory.cpp
@@ -2,27 +2,34 @@
 #include "GwenUtil/ControlFactory.h"
 #include "Gwen/Controls.h"
 
+class DesignerCanvas;
+
 namespace Gwen
 {
     namespace ControlFactory
     {
-//        class DesignerCanvas_Factory : public Gwen::ControlFactory::Base
-//        {
-//        public:
-//
-//            GWEN_CONTROL_FACTORY_FOR(DesignerCanvas, Base)
-//            {
-//            }
-//
-//            virtual Gwen::Controls::Base* CreateInstance(Gwen::Controls::Base* parent)
-//            {
-//                return NULL;
-//            }
-//
-//        };
-//
-//
-//        GWEN_CONTROL_FACTORY(DesignerCanvas_Factory);
-//
+        
+        // This is a container for saving designer content.
+        // TODO - Move this to the designer code, or remove.
+        class DesignerCanvas_Factory : public Gwen::ControlFactory::Base
+        {
+        public:
+
+            GWEN_CONTROL_FACTORY_DETAILS(DesignerCanvas, DesignerCanvas, Base)
+
+            GWEN_CONTROL_FACTORY_CONSTRUCTOR(DesignerCanvas_Factory, Base)
+            {
+            }
+
+            virtual Gwen::Controls::Base* CreateInstance(Gwen::Controls::Base* parent)
+            {
+                return NULL;
+            }
+
+        };
+
+
+        GWEN_CONTROL_FACTORY(DesignerCanvas_Factory);
+
     }
 }
