0894165ceab3bdeca0080059d9285701ce68d315
BQ <chinbillybilbo@gmail.com>
2016-02-22 14:51:37 +0000

    make SFML2 gwork build static library.

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -1,26 +1,32 @@
 
-## Configuration options
+#### Project Configuration
 
 # setup version numbers
-set(VERSION_MAJOR 1)
-set(VERSION_MINOR 0)
+set(VERSION_MAJOR 0)
+set(VERSION_MINOR 1)
 set(VERSION_PATCH 0)
 set(VERSION_STR "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
 message("Project version: ${VERSION_STR}")
 
+### User options
+
 # Windows only
 if(WIN32)
-    option(BUILD_RENDER_DIRECT2D    "Renderer: Direct2D" OFF)
-    option(BUILD_RENDER_DIRECTX9    "Renderer: DirectX9" OFF)
-    option(BUILD_RENDER_GDIPLUS     "Renderer: GDIPlus" OFF)
+    option(RENDER_DIRECT2D  "Renderer: Direct2D" OFF)
+    option(RENDER_DIRECTX9  "Renderer: DirectX9" OFF)
+    option(RENDER_GDIPLUS   "Renderer: GDIPlus" OFF)
 endif()
 
 # Cross-platform
-option(BUILD_RENDER_ALLEGRO5    "Renderer: Allegro5" OFF)
-option(BUILD_RENDER_OPENGL      "Renderer: OpenGL" OFF)
-option(BUILD_RENDER_SDL2        "Renderer: SDL2" OFF)
-option(BUILD_RENDER_SFML        "Renderer: SFML" OFF)
-option(BUILD_RENDER_SFML2       "Renderer: SFML2" OFF)
+option(RENDER_ALLEGRO5      "Renderer: Allegro5" OFF)
+option(RENDER_OPENGL        "Renderer: OpenGL" OFF)
+option(RENDER_SDL2          "Renderer: SDL2" OFF)
+option(RENDER_SFML          "Renderer: SFML" OFF)
+option(RENDER_SFML2         "Renderer: SFML2" OFF)
+
+option(BUILD_TEST           "Include unittests" ON)
+option(BUILD_SAMPLE         "Include sample" ON)
+
 
 if(WIN32)
     set(BUILD_PLATFORM "Windows")
@@ -35,19 +41,12 @@ if(NOT CMAKE_BUILD_TYPE)
     )
 endif()
 
-# Add an option for choosing the build type (shared or static)
-if(NOT BUILD_SHARED_LIBS)
-    set(BUILD_SHARED_LIBS TRUE
-        CACHE BOOL "TRUE to build Gwork as a shared library, FALSE to build it as a static library."
-    )
-endif()
-
-# Set the default test build option to false
-if(NOT BUILD_TEST)
-    set(BUILD_TEST FALSE
-        CACHE BOOL "TRUE to build the unit tests, FALSE otherwise."
-    )
-endif()
+# TODO: Add an option for choosing the build type (shared or static)
+# if(NOT BUILD_SHARED_LIBS)
+#     set(BUILD_SHARED_LIBS TRUE
+#         CACHE BOOL "TRUE to build Gwork as a shared library, FALSE to build it as a static library."
+#     )
+# endif()
 
 # define install directory for miscelleneous files
 if(WIN32 AND NOT UNIX)
@@ -56,11 +55,25 @@ elseif(UNIX)
     set(INSTALL_MISC_DIR share/gwork)
 endif()
 
+if(BUILD_TEST)
+    message("Including tests")
+endif(BUILD_TEST)
+    
+if(BUILD_SAMPLE)
+    message("Including sample")
+endif(BUILD_SAMPLE)
 
-if (BUILD_RENDER_SDL2)
+if(RENDER_SDL2)
     find_package(SDL2 REQUIRED)
     message("Using SDL2")
     set(RENDERER_INC "${SDL2_INCLUDE_DIRS}")
     set(RENDERER_LIB "${SDL2_LIBRARIES}")
-endif(BUILD_RENDER_SDL2)
+endif(RENDER_SDL2)
+
+if(RENDER_SFML2)
+    find_package(SFML2 REQUIRED)
+    message("Using SFML2")
+    set(RENDERER_INC "${SFML2_INCLUDE_DIRS}")
+    set(RENDERER_LIB "${SFML2_LIBRARIES}")
+endif(RENDER_SFML2)
 
