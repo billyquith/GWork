0d1f83661df58a9ffab6dfa8554d5c79333e9321
BQ <chinbillybilbo@gmail.com>
2016-08-16 00:54:29 +0100

    Simplify args.

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -5,15 +5,16 @@ set -ev
 
 echo "Lang:$LANG, OS:$TRAVIS_OS_NAME, CC:$CC"
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+function install_osx {
     brew update
-    #if [[ -z `brew list | grep doxygen` ]]; then brew install doxygen; fi
+    if [[ -z `brew list | grep doxygen` ]]; then brew install doxygen; fi
     if [[ -z `brew list | grep cmake` ]]; then brew install cmake; fi
     brew outdated cmake || brew upgrade cmake
-fi
+}
   
-# if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-#     #sudo apt-get -qq update
-#     #sudo apt-get install -y cmake doxygen
-# fi
+function install_linux {
+    sudo apt-get -qq update
+    sudo apt-get install -y cmake doxygen
+}
 
+install_$TRAVIS_OS_NAME
