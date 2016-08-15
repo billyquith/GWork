da0c8ded78a2097848ce83ef8da041c9a979285c
BQ <chinbillybilbo@gmail.com>
2016-08-16 00:30:14 +0100

    Set all env variables in yaml - source seems to have issues

--- ci/travis/make_build.sh
+++ ci/travis/make_build.sh
@@ -4,13 +4,13 @@
 set -ev
 
 cmake --version
-echo "CMAKE_ARGS:${CMAKE_ARGS}"
+echo "CMAKE_ARGS:$CMAKE_ARGS"
 mkdir build && cd build
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cmake .. -GXcode $CMAKE_ARGS
+    cmake .. -GXcode "$CMAKE_ARGS"
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    cmake .. -G "Unix Makefiles" $CMAKE_ARGS
+    cmake .. -G "Unix Makefiles" "$CMAKE_ARGS"
 fi
