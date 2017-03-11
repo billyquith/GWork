35f09e9d4a10ca3729286bb50dc49041fa212900
BQ <chinbillybilbo@gmail.com>
2017-03-11 14:53:50 +0000

    Find NuGet packages for Allegro5 on Windows.

--- cmake/Modules/FindAllegro5.cmake
+++ cmake/Modules/FindAllegro5.cmake
@@ -3,6 +3,10 @@
 set(ALLEGRO_VERSION "5.2")  # Required version
 message("Looking for Allegro ${ALLEGRO_VERSION}")
 
+# Allegro binaries can get retrieved from NuGet for Windows.
+#   - MSVC: Tools > NuGet Package Manager > Package Manager Console
+#   - PM> Install-Package Allegro
+
 # Find headers
 find_path(ALLEGRO_INCLUDE
     NAMES
@@ -12,6 +16,7 @@ find_path(ALLEGRO_INCLUDE
         /usr/local/include
         /opt/local/include
         /sw/include
+        ${CMAKE_BINARY_DIR}/packages/Allegro.5.*/build/native/include  # NuGet
 )
 
 # Components to find
@@ -27,6 +32,12 @@ set(ALLEGRO_LIB_LIST
 
 mark_as_advanced(ALLEGRO_INCLUDE ALLEGRO_LIB_LIST)
 
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(AL_LIB_TYPE v140/x64)
+else()
+    set(AL_LIB_TYPE v140/win32)
+endif()
+
 # Find components
 foreach(ALLEGRO_LIB_EACH ${ALLEGRO_LIB_LIST})    
     find_library(${ALLEGRO_LIB_EACH}_LIB
@@ -38,6 +49,7 @@ foreach(ALLEGRO_LIB_EACH ${ALLEGRO_LIB_LIST})
             /usr/local/lib
             /opt/local/lib
             /sw/lib
+            ${CMAKE_BINARY_DIR}/packages/Allegro.5.*/build/native/${AL_LIB_TYPE}/lib  # NuGet
     )
     message("Component: ${ALLEGRO_LIB_EACH} -> ${${ALLEGRO_LIB_EACH}_LIB}")
     list(APPEND ALLEGRO_LIBRARIES ${${ALLEGRO_LIB_EACH}_LIB})
