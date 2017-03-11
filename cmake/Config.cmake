317a1090e8cc1da8a6fc07c5b524dcc7eb06e3f5
BQ <chinbillybilbo@gmail.com>
2017-03-11 13:56:54 +0000

    Direct2D builds and runs. - Colour glitches. - Not rendering yet!

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -1,25 +1,21 @@
 
 #### Project Configuration
 
-# setup version numbers
+# Version
 set(GWK_VERSION_MAJOR 0)
 set(GWK_VERSION_MINOR 2)
 set(GWK_VERSION_PATCH 0)
 set(GWK_VERSION_STR "${GWK_VERSION_MAJOR}.${GWK_VERSION_MINOR}.${GWK_VERSION_PATCH}")
 message("Project version: ${GWK_VERSION_STR}")
 
-### User options
-
-set(GWK_PLATFORM_NAME "Null")
+#-----------------------------------------------------------
+# User options
 
 # Windows only
 if(WIN32)
-    # TODO: option(RENDER_DIRECT2D  "Renderer: Direct2D" OFF)
+    option(RENDER_DIRECT2D  "Renderer: Direct2D" OFF)
     # TODO: option(RENDER_DIRECTX9  "Renderer: DirectX9" OFF)
-    # TODO: option(RENDER_GDIPLUS   "Renderer: GDIPlus" OFF)
-    
-    # For Windows platform:
-    # set(GWK_PLATFORM_NAME "Windows")    
+    # TODO: option(RENDER_GDIPLUS   "Renderer: GDIPlus" OFF)    
 endif()
 
 # Cross-platform
@@ -34,6 +30,11 @@ option(BUILD_SAMPLE         "Include sample" ON)
 
 # This is for development but can be used by the user.
 option(ALLOC_STATS "Track memory allocations" OFF)
+
+#-----------------------------------------------------------
+# Configure once options known
+
+set(GWK_PLATFORM_NAME "Null")   # default/fallback platform
 
 # Set the default build type to release with debug info
 if(NOT CMAKE_BUILD_TYPE)
@@ -65,14 +66,35 @@ if(BUILD_SAMPLE)
     message("Including sample")
 endif(BUILD_SAMPLE)
 
+#-----------------------------------------------------------
+# Renderer config
+#   - Order alphabetic.
 
 if(RENDER_ALLEGRO5)
-    find_package(Allegro5 REQUIRED)
     set(GWK_RENDER_NAME "Allegro5")
+    find_package(Allegro5 REQUIRED)
     set(GWK_RENDER_INCLUDES "${ALLEGRO5_INCLUDE_DIRS}")
     set(GWK_RENDER_LIBRARIES "${ALLEGRO5_LIBRARIES}")
     set(GWK_PLATFORM_NAME "Allegro5")
 endif(RENDER_ALLEGRO5)
+
+if(RENDER_DIRECT2D)
+    set(GWK_RENDER_NAME "Direct2D")
+    find_package(Direct2D REQUIRED)
+    set(GWK_RENDER_INCLUDES "${DIRECT2D_INCLUDE_DIRS}")
+    set(GWK_RENDER_LIBRARIES "${DIRECT2D_LIBRARIES}")
+    # find_package(DirectX REQUIRED)
+    # set(GWK_RENDER_INCLUDES "${DirectX_D2D1_INCLUDE_DIR}")
+    # set(GWK_RENDER_LIBRARIES "${DirectX_D2D1_LIBRARY}")
+    # set(GWK_PLATFORM_NAME "Windows")
+    set(GWK_INPUT_NAME "Windows")
+endif(RENDER_DIRECT2D)
+
+if(RENDER_NULL)
+    set(GWK_RENDER_NAME "Null")
+    set(GWK_RENDER_INCLUDES "")
+    set(GWK_RENDER_LIBRARIES )
+endif(RENDER_NULL)
 
 if(RENDER_OPENGL)
     find_package(GLFW REQUIRED)
@@ -106,12 +128,12 @@ if(RENDER_SFML2)
     set(GWK_RENDER_LIBRARIES ${SFML_LIBRARIES} ${SFML_DEPENDENCIES})
 endif(RENDER_SFML2)
 
-if(RENDER_NULL)
-    set(GWK_RENDER_NAME "Null")
-    set(GWK_RENDER_INCLUDES "")
-    set(GWK_RENDER_LIBRARIES )
-endif(RENDER_NULL)
+#-----------------------------------------------------------
+# Sanity checks and summary
 
+if(NOT GWK_INPUT_NAME)
+    set(GWK_INPUT_NAME ${GWK_RENDER_NAME})
+endif()
 
 if(NOT GWK_RENDER_NAME)
     message(FATAL_ERROR "No renderer was specified. See RENDER_<name> options.")
