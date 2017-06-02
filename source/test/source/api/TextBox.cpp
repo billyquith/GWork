f378ce9c714a0c397b1bf523145d467efa633671
Hayden Curran <haydenz@gmail.com>
2017-06-03 11:02:32 +1200

    Remove extra TextBoxMultiline test, add SetText past length limit to previous test

--- source/test/source/api/TextBox.cpp
+++ source/test/source/api/TextBox.cpp
@@ -85,14 +85,8 @@ public:
             Gwk::Controls::TextBoxMultiline* label = new Gwk::Controls::TextBoxMultiline(this);
             label->SetText("Max Length 20");
             label->SetMaxTextLength(20);
-            label->SetPos(220, 195);
-            label->SetSize(200, 45);
-        }
-        {
-            Gwk::Controls::TextBoxMultiline* label = new Gwk::Controls::TextBoxMultiline(this);
-            label->SetMaxTextLength(20);
             label->SetText("Max Length 20 -- Set text longer than max length");
-            label->SetPos(220, 245);
+            label->SetPos(220, 195);
             label->SetSize(200, 45);
         }
     }
