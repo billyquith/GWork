ef819886b20df06febdecb830d34d9e370659480
BQ <chinbillybilbo@gmail.com>
2017-11-04 12:20:37 +0000

    Software renderer draw fonts.
    - Viewer added in sample.

--- source/platform/renderers/Software/Software.cpp
+++ source/platform/renderers/Software/Software.cpp
@@ -7,6 +7,10 @@
 #include <Gwork/Renderers/Software.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include <Gwork/External/stb_image.h>
+#define STB_TRUETYPE_IMPLEMENTATION
+#define STBTT_STATIC
+#include <Gwork/External/stb_truetype.h>
+#include <sys/stat.h>
 
 namespace Gwk
 {
@@ -62,17 +66,55 @@ namespace Drawing
     
 //-------------------------------------------------------------------------------
 
+// See "Font Size in Pixels or Points" in "stb_truetype.h"
+static constexpr float c_pixToPoints = 1.333f;
+static constexpr int c_texsz = 256;   // TODO - fix this hack.
+
 Font::Status SoftwareResourceLoader::LoadFont(Font& font)
 {
     const String fontFile = m_paths.GetPath(ResourcePaths::Type::Font, font.facename);
+    
+    FILE* f = fopen(fontFile.c_str(), "rb");
+    if (!f)
+    {
+        font.data = nullptr;
+        font.status = Font::Status::ErrorFileNotFound;
+        return font.status;
+    }
+    
+    struct stat finfo;
+    const int rc = stat(fontFile.c_str(), &finfo);
+    const size_t fsz = rc == 0 ? finfo.st_size : -1;
+    assert(fsz > 0);
+    
+    unsigned char* ttfdata = new unsigned char[fsz];
+    fread(ttfdata, 1, fsz, f);
+    fclose(f);
+    
+    unsigned char *font_bmp = new unsigned char[c_texsz * c_texsz];
+    
+    font.render_data = new stbtt_bakedchar[96];
+    
+    stbtt_BakeFontBitmap(ttfdata, 0,
+                         font.realsize * c_pixToPoints, // height
+                         font_bmp,
+                         c_texsz, c_texsz,
+                         32,96,             // range to bake
+                         static_cast<stbtt_bakedchar*>(font.render_data));
+    delete [] ttfdata;
+    
+    font.data = font_bmp;
+    font.status = Font::Status::Loaded;
     
     return font.status;
 }
 
 void SoftwareResourceLoader::FreeFont(Gwk::Font& font)
 {
-    if (font.status == Font::Status::Loaded)
+    if (font.IsLoaded())
     {
+        delete [] static_cast<unsigned char*>(font.data);
+        delete [] static_cast<stbtt_bakedchar*>(font.render_data);
         font.status = Font::Status::Unloaded;
     }
 }
@@ -133,18 +175,99 @@ void Software::SetDrawColor(Gwk::Color color)
     m_color = color;
 }
 
-void Software::RenderText(Gwk::Font* font, Gwk::Point pos, const Gwk::String& text)
-{
-    if (!EnsureFont(*font))
-        return;    
-}
-
 Gwk::Point Software::MeasureText(Gwk::Font* font, const Gwk::String& text)
 {
     if (!EnsureFont(*font))
         return Gwk::Point(0, 0);
+    
+    Point sz(0, font->realsize * c_pixToPoints);
+    
+    float x = 0.f, y = 0.f;
+    const char *pc = text.c_str();
+    size_t slen = text.length();
+    
+    while (slen > 0)
+    {
+        if (*pc >= 32 && *pc <= 127)
+        {
+            stbtt_aligned_quad q;
+            stbtt_GetBakedQuad(static_cast<stbtt_bakedchar*>(font->render_data),
+                               c_texsz,c_texsz,
+                               *pc - 32,
+                               &x, &y, &q, 1); // 1=opengl & d3d10+,0=d3d9
+            
+            sz.x = q.x1;
+            sz.y = std::max(sz.y, int((q.y1 - q.y0) * c_pixToPoints));
+        }
+        ++pc, --slen;
+    }
+    
+    return sz;
+}
 
-    return Point(0,0);
+static Color BlendAlpha(Color const& src, Color const& dst)
+{
+    const float a = src.a * (1.f/255.f);
+    const float b = 1.f - a;
+    const Color r(src.r * a + dst.r * b,
+                  src.g * a + dst.g * b,
+                  src.b * a + dst.b * b,
+                  src.a);
+    return r;
+}
+    
+void Software::RenderText(Gwk::Font* font, Gwk::Point pos, const Gwk::String& text)
+{
+    if (!EnsureFont(*font))
+        return;
+
+    int ix = pos.x, iy = pos.y;
+    Translate(ix, iy);
+    
+    float x = ix, y = iy;
+
+    const char *pc = text.c_str();
+    size_t slen = text.length();
+    const float height = font->realsize * c_pixToPoints;
+    const unsigned char * const fontBmp = static_cast<const unsigned char*>(font->data);
+    
+    while (slen > 0)
+    {
+        if (*pc >= 32 && *pc <= 127)
+        {
+            stbtt_aligned_quad q;
+            stbtt_GetBakedQuad(static_cast<stbtt_bakedchar*>(font->render_data),
+                               c_texsz, c_texsz,
+                               *pc - 32,
+                               &x, &y, &q, 1); // 1=opengl & d3d10+,0=d3d9
+            
+            const Rect r(q.x0, height + q.y0, q.x1 - q.x0, q.y1 - q.y0);
+            
+//            DrawTexturedRect(&tex, r, q.s0,q.t0, q.s1,q.t1);
+            
+//            void TexturedRect(PixelBuffer& pb, const PixelBuffer& pbsrc,
+//                              const Gwk::Rect& rect, float u1, float v1, float u2, float v2)
+            {
+                const Point srcsz(c_texsz, c_texsz);
+                const Point uvtl(srcsz.x * q.s0, srcsz.y * q.t0);
+                Color col(m_color);
+                
+                const float dv = (q.t1 - q.t0) * srcsz.y / r.h;
+                for (int fy = 0; fy < r.h; ++fy)
+                {
+                    const float du = (q.s1 - q.s0) * srcsz.x / r.w;
+                    for (int fx = 0; fx < r.w; ++fx)
+                    {
+                        const Point fp(uvtl.x + du*fx, uvtl.y + dv*fy);
+                        const unsigned char fi = fontBmp[fp.y * c_texsz + fp.x];
+                        col.a = fi;
+                        m_pixbuf->At(x + fx, y + fy) = BlendAlpha(col, m_pixbuf->At(x + fx, y + fy));
+                    }
+                }
+            }
+        }
+        ++pc, --slen;
+    }
 }
 
 void Software::StartClip()
--- source/samples/Software/SoftwareSample.cpp
+++ source/samples/Software/SoftwareSample.cpp
@@ -4,20 +4,37 @@
  *  See license in Gwork.h
  */
 
-#include <Gwork/Gwork.h>
 #include <Gwork/Skins/Simple.h>
 #include <Gwork/Skins/TexturedBase.h>
-#include <Gwork/Test/TestAPI.h>
 #include <Gwork/Renderers/Software.h>
 #include <Gwork/Platform.h>
+#include <Gwork/Test/TestAPI.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <Gwork/External/stb_image_write.h>
 
+#define VIEWER 1    // turn on for interactive viewer
+
+#if VIEWER
+#   include <Gwork/Input/SDL2.h>
+#endif
 
 int main(int argc, char** argv)
 {
     // Setup our window and renderer
     const Gwk::Point screenSize(1024, 768);
+
+#if VIEWER
+    // We'll use SDL2 for the window and rendering management
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        return EXIT_FAILURE;
+    
+    SDL_Window *window = SDL_CreateWindow("Gwork Software Sample", 100, 100,
+                                          screenSize.x, screenSize.y, SDL_WINDOW_SHOWN);
+    if (!window)
+        return EXIT_FAILURE;
+    
+    SDL_Renderer *sdlRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+#endif
 
     Gwk::Platform::RelativeToExecutablePaths paths(GWORK_RESOURCE_DIR);
     Gwk::Renderer::SoftwareResourceLoader loader(paths);
@@ -45,16 +62,59 @@ int main(int argc, char** argv)
     // Create our unittest control (which is a Window with controls in it)
     TestAPI* unit = new TestAPI(canvas);
     unit->SetPos(10, 10);
-
+    
+#if VIEWER
+    Gwk::Input::SDL2 input;
+    input.Initialize(canvas);
+    auto *texture = SDL_CreateTexture(sdlRenderer,
+                                      SDL_PIXELFORMAT_ABGR8888,
+                                      SDL_TEXTUREACCESS_STREAMING,
+                                      screenSize.x, screenSize.y);
+    bool haveQuit = false;
+    while (!haveQuit)
+    {
+        SDL_Event evt;
+        while (SDL_PollEvent(&evt))
+        {
+            if (evt.type == SDL_QUIT)
+                haveQuit = true;
+            
+            input.ProcessEvent(&evt);
+        }
+        
+        // render the GUI to our buffer
+        renderer->BeginContext(nullptr);
+        canvas->RenderCanvas();
+        renderer->PresentContext(nullptr);
+        renderer->EndContext(nullptr);
+        
+        // show the software rendered GUI on the screen
+        SDL_UpdateTexture(texture, NULL, &pixbuff.At(0,0), screenSize.x*4);
+        SDL_RenderClear(sdlRenderer);
+        SDL_RenderCopy(sdlRenderer, texture, NULL, NULL);
+        SDL_RenderPresent(sdlRenderer);
+        SDL_Delay(5);
+    }
+#else
+    // render GUI to buffer
     renderer->BeginContext(nullptr);
     canvas->RenderCanvas();
     renderer->PresentContext(nullptr);
     renderer->EndContext(nullptr);
     
+    // write screen shot
     stbi_write_png("sw.png", screenSize.x, screenSize.y, 4, &pixbuff.At(0,0), 4*screenSize.x);
+#endif
     
-    delete unit;
-    delete renderer;
+//    skin.ReleaseFont(skin.GetDefaultFont());
+//    delete unit;
+//    delete renderer;
+    
+#if VIEWER
+    SDL_DestroyTexture(texture);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+#endif
     
     return EXIT_SUCCESS;
 }
