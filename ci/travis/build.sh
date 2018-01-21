30fdf808331a47b983c90909530bc3f297dfb084
BQ <chinbillybilbo@gmail.com>
2018-01-21 12:07:34 +0000

    More minimal compiler selection.

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -8,11 +8,10 @@ env
 
 BUILD_OS=$TRAVIS_OS_NAME
 #CPP_COMPILER=$1
-WANT_OPTS="-DWANT_TESTS=ON -DWANT_SAMPLE=ON -DRENDER_NULL=ON $1"
+WANT_OPTS="-DWANT_TESTS=ON -DWANT_SAMPLE=ON -DRENDER_NULL=ON $FEATURES"
 
 cmake --version
-echo "CXX=$CXX CC=$CC"
-# echo "C++ compiler: $CPP_COMPILER"
+echo "Using C++ compiler: CXX=$CXX, CC=$CC"
 echo "Options for cmake generation: $WANT_OPTS"
 
 
@@ -24,24 +23,12 @@ function prepare_osx
     popd
 }
 
-# map C++ compiler to C compiler
-function cpp2c
-{
-    local comp=$1
-    comp=${comp/clang\+\+/clang}
-    comp=${comp/g\+\+/gcc}
-    echo $comp
-}
-
 function prepare_linux
 {
     mkdir build
     pushd build
     # Travis doesn't pass on the COMPILER version so we'll use env CXX variable
-    local comp=$1
-    local ccomp=$(cpp2c $comp)
-    echo "Requesting C compiler: $ccomp, C++ compiler: $comp"
-    local ccmd="CC=$ccomp CXX=$comp cmake .. -G ""Unix Makefiles"" $WANT_OPTS"
+    local ccmd="cmake .. -G \"Unix Makefiles\" $WANT_OPTS"
     echo "$ccmd"
     eval "$ccmd"
     popd
